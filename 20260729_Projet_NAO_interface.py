4f56c1333af1e364ffc2d7fe9ba222fd9532ab2d
Kidriix <alexis.blumenau@proton.me>
2026-07-29 18:22:35 +0200

    Update 20260729_Projet_NAO_interface.py
    List in selectbox sorted in alphabetical order

--- 20260729_Projet_NAO_interface.py
+++ 20260729_Projet_NAO_interface.py
@@ -15,7 +15,7 @@ grids = pd.read_csv("20260728_Projet_NAO_results.csv", sep=";", encoding='utf-8'
 truth_table = pd.read_csv("20260728_Projet_NAO_table_verite.csv", sep=";", encoding='latin-1')
 legend = pd.read_csv("20260728_Projet_NAO_legende.csv", sep=";", encoding='latin-1')
 
-stations_names = [" "] + truth_table["Nom arret"].tolist()
+stations_names = [" "] + sorted(truth_table["Nom arret"].tolist())
 st.session_state.stations_names = stations_names
 
 def pick_random_grid():
