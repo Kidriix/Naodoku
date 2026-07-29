eb5b84ff56167a84b442c85ceadd70930d4552c9
Kidriix <alexis.blumenau@proton.me>
2026-07-29 16:01:10 +0200

    Add files via upload

---
+++ 20260728_Projet_NAO_generate.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from itertools import combinations, permutations
+
+
+
+truth_table = pd.read_csv("H:\\Perso\\Naolib\\20260728_Projet_NAO_table_verite.csv", sep=";", encoding="latin-1")
+pairs = pd.read_csv("H:\\Perso\\Naolib\\20260728_Projet_NAO_paires.csv", sep=";", encoding="latin-1")
+
+stations_names = truth_table["Nom arret"].tolist()
+criteria_names = np.unique(pairs["Paire 1"].tolist())
+
+all_criteria_comb = [comb for comb in combinations(criteria_names, 6)]
+output_cols = ["Criteria A", "Criteria B", "Criteria C", "Criteria D", "Criteria E", "Criteria F", "Stations AD", "Stations AE", "Stations AF", "Stations BD", "Stations BE", "Stations BF", "Stations CD", "Stations CE", "Stations CF"]
+result = pd.DataFrame(columns=output_cols)
+
+def is_valid_grid(grid):
+    res = True
+
+    colA, colB, colC, rowD, rowE, rowF = grid
+    nb_res = []
+    for col in [colA, colB, colC]:
+        for row in [rowD, rowE, rowF]:
+            nb = pairs[((pairs["Paire 1"]==str(col))) & (pairs["Paire 2"]==str(row)) | ((pairs["Paire 2"]==str(col)) & (pairs["Paire 1"]==str(row)))]["Nb arrets"].tolist()[0]
+            if nb == 0:
+                res = False
+                return res, nb_res
+            nb_res.append(nb)
+    return res, nb_res
+
+#nb_comb = len(all_criteria_comb)
+for (i, combination) in enumerate(all_criteria_comb):
+    if i % 100 ==0 :
+        print(i)
+
+    (A, B, C, D, E, F) = combination
+    all_comb_pairs = [comb for comb in combinations(combination, 2)]
+    nb_stations_pairs = [pairs[((pairs["Paire 1"]==str(crit1)) & (pairs["Paire 2"]==str(crit2))) | ((pairs["Paire 2"]==str(crit1)) & (pairs["Paire 1"]==str(crit2)))]["Nb arrets"].tolist()[0] for (crit1, crit2) in all_comb_pairs]
+
+    nb_zeros = nb_stations_pairs.count(0)
+    nb_non_zeros = len(nb_stations_pairs)-nb_zeros
+
+    if nb_non_zeros >= 9 :
+    
+        comb_wo_A = (B, C, D, E, F)
+        all_pairs_without_A = [comb for comb in combinations(comb_wo_A, 2)]
+        all_triplets_with_A = [ (A, p1, p2) for (p1, p2) in all_pairs_without_A]
+        all_other_triplets = [tuple(list(filter(lambda x: (x != p1) & (x != p2), comb_wo_A))) for (p1, p2) in all_pairs_without_A]
+        for (triplet_A, triplet_other) in zip(all_triplets_with_A, all_other_triplets):
+            grid = triplet_A + triplet_other
+            valid_grid, nb_grid = is_valid_grid(grid)
+            if valid_grid :
+                row_result = {
+                    "Criteria A": str(triplet_A[0]), 
+                    "Criteria B": str(triplet_A[1]),
+                    "Criteria C": str(triplet_A[2]), 
+                    "Criteria D": str(triplet_other[0]), 
+                    "Criteria E": str(triplet_other[1]), 
+                    "Criteria F": str(triplet_other[2]),  
+                    "Stations AD": nb_grid[0], 
+                    "Stations AE": nb_grid[1], 
+                    "Stations AF": nb_grid[2], 
+                    "Stations BD": nb_grid[3], 
+                    "Stations BE": nb_grid[4], 
+                    "Stations BF": nb_grid[5], 
+                    "Stations CD": nb_grid[6], 
+                    "Stations CE": nb_grid[7], 
+                    "Stations CF": nb_grid[8], 
+                }
+                result.loc[len(result)] = row_result
+    if len(result) > 10000:
+        break
+
+print(len(result))
+pass
+result.to_csv("H:\\Perso\\Naolib\\20260728_Projet_NAO_results.csv", sep=";", encoding='utf-8')
+
---
+++ 20260729_Projet_NAO_interface.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import altair as alt
+import streamlit as st
+import os
+import numpy as np
+from datetime import datetime
+
+st.cache_data.clear()
+submitted = False
+
+st.set_page_config(page_title="DEMO", layout="wide")
+
+refresh = False
+grids = pd.read_csv("H:\\Perso\\Naolib\\20260728_Projet_NAO_results.csv", sep=";", encoding='utf-8')
+truth_table = pd.read_csv("H:\\Perso\\Naolib\\20260728_Projet_NAO_table_verite.csv", sep=";", encoding='latin-1')
+
+stations_names = [" "] + truth_table["Nom arret"].tolist()
+st.session_state.stations_names = stations_names
+
+def pick_random_grid():
+    a = np.random.randint(len(grids))
+    selected_grid = grids.iloc[int(a)]
+
+    # transpose = np.random.randint(2)
+    # if transpose == 1:
+    #   selected_grid.swaplevel(1,4)
+    #   selected_grid.swaplevel(2,5)
+    #   selected_grid.swaplevel(3,6)
+
+    return selected_grid
+
+if "grid" not in st.session_state:
+    st.session_state.grid = pick_random_grid()
+
+def is_cell_okay(crit_col, crit_row, cell):
+  crit_col_status = truth_table[truth_table["Nom arret"] == cell][crit_col].tolist()[0]
+  crit_row_status = truth_table[truth_table["Nom arret"] == cell][crit_row].tolist()[0]
+  if (crit_col_status == "VRAI") & (crit_row_status == "VRAI"):
+     return True
+  else : 
+     return False
+  
+def is_grid_okay():
+   
+  for col in ["A", "B", "C"]:
+    col_name = st.session_state.grid[f"Criteria {col}"]
+    for row in ["D", "E", "F"]:
+      row_name = st.session_state.grid[f"Criteria {row}"]
+      cell = st.session_state.__getattr__(col+row)
+      cell_status = is_cell_okay(col_name, row_name, cell)
+      if not cell_status:
+        st.session_state.grid_status = False
+        st.write("Dommage, au moins 1 erreur :(")
+        return
+  st.session_state.grid_status = True
+  st.write("Bravo !")
+
+def update_stations_list():
+  for col in ["A", "B", "C"]:
+    for row in ["D", "E", "F"]:
+      cell = st.session_state.__getattr__(col+row)
+      if cell != " ":
+        stations_options.remove(cell)
+  return stations_options
+
+with st.form("Nouvelle partie de NAODOKU !"):
+  st.write("Nouvelle partie de NAODOKU !")
+
+  stations_options = st.session_state.stations_names
+
+  for row in range(4):
+    cols = st.columns(4)
+    if row == 0:
+      cols[1].write(st.session_state.grid["Criteria A"])
+      cols[2].write(st.session_state.grid["Criteria B"])
+      cols[3].write(st.session_state.grid["Criteria C"])
+    elif row == 1:
+      cols[0].write(st.session_state.grid["Criteria D"])
+      AD = cols[1].selectbox(label=f"{st.session_state.grid["Stations AD"]} arrêts possibles", options=stations_options, key="AD")
+      BD = cols[2].selectbox(label=f"{st.session_state.grid["Stations BD"]} arrêts possibles", options=stations_options, key="BD")
+      CD = cols[3].selectbox(label=f"{st.session_state.grid["Stations CD"]} arrêts possibles", options=stations_options, key="CD")
+    elif row == 2:
+      cols[0].write(st.session_state.grid["Criteria E"])
+      AE = cols[1].selectbox(label=f"{st.session_state.grid["Stations AE"]} arrêts possibles", options=stations_options, key="AE")
+      BE = cols[2].selectbox(label=f"{st.session_state.grid["Stations BE"]} arrêts possibles", options=stations_options, key="BE")
+      CE = cols[3].selectbox(label=f"{st.session_state.grid["Stations CE"]} arrêts possibles", options=stations_options, key="CE")
+    elif row == 3:
+      cols[0].write(st.session_state.grid["Criteria F"])
+      AF = cols[1].selectbox(label=f"{st.session_state.grid["Stations AF"]} arrêts possibles", options=stations_options, key="AF")
+      BF = cols[2].selectbox(label=f"{st.session_state.grid["Stations BF"]} arrêts possibles", options=stations_options, key="BF")
+      CF = cols[3].selectbox(label=f"{st.session_state.grid["Stations CF"]} arrêts possibles", options=stations_options, key="CF")
+
+  submitted = st.form_submit_button(label="Vérifier la grille")
+  if submitted :
+    is_grid_okay()
+
+if st.button("Nouvelle grille"):
+    st.session_state.grid = pick_random_grid()
+    st.rerun()
