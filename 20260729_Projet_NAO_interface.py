c78f362e8fd03ebc1b6a0a778e4a11b4a16a01a4
Kidriix <alexis.blumenau@proton.me>
2026-08-02 02:00:45 +0200

    Update 20260729_Projet_NAO_interface.py

--- 20260729_Projet_NAO_interface.py
+++ 20260729_Projet_NAO_interface.py
@@ -9,10 +9,13 @@ import base64
 
 st.set_page_config(page_title="NAO_DEMO", layout="centered")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 grids = pd.read_csv("20260728_Projet_NAO_results.csv", sep=";", encoding='utf-8')
 truth_table = pd.read_csv("20260728_Projet_NAO_table_verite.csv", sep=";", encoding='latin-1')
 legend = pd.read_csv("20260728_Projet_NAO_legende.csv", sep=";", encoding='latin-1')
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+stats_save_file = os.path.join(BASE_DIR, ".stats", "Projet_NAO_stats.csv")
+
 lines_logos = [
     os.path.join(BASE_DIR, ".images", f"indices de ligne_svg_picto ligne {i}.svg")
     for i in range(1, 6)
@@ -29,6 +32,8 @@ BUS_SVG = '''<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="
 <path d="M15.7501 15.3799C15.2344 15.3799 14.8165 15.8018 14.8165 16.3225C14.8165 16.8431 15.2344 17.265 15.7501 17.265C16.2657 17.265 16.6836 16.8431 16.6836 16.3225C16.6836 15.8018 16.2657 15.3799 15.7501 15.3799Z" fill="#000000"/>
 <path d="M21.3001 6.40594H20.184V5.47244C20.1823 3.55773 18.6353 1.99915 16.7345 1.99915H7.2671C5.36555 1.99997 3.81766 3.55773 3.81684 5.47326V6.40676H2.69992C2.31336 6.40676 2 6.72012 2 7.10586V8.99836C2 9.38492 2.31336 9.69746 2.69992 9.69746H3.81437L3.81026 16.6704C3.81026 17.5973 4.16721 18.4699 4.81696 19.1263C5.07275 19.3845 5.36143 19.5959 5.67315 19.7596V21.3C5.67315 21.6866 5.98651 21.9991 6.37307 21.9991H7.30493C7.69149 21.9991 8.00485 21.6858 8.00485 21.3V20.1469H15.9943V21.3C15.9943 21.6866 16.3077 21.9991 16.6942 21.9991H17.6261C18.0127 21.9991 18.326 21.6858 18.326 21.3V19.7596C18.6377 19.5967 18.9273 19.3845 19.1822 19.1263C19.832 18.4699 20.1897 17.5973 20.1889 16.6704L20.1848 9.69746H21.2993C21.6858 9.69746 21.9992 9.3841 21.9992 8.99836V7.10586C21.9992 6.7193 21.6858 6.40676 21.2993 6.40676L21.3001 6.40594ZM7.26627 3.39817H16.7337C17.863 3.39817 18.7825 4.3292 18.7833 5.47326L18.785 8.4029C18.6879 9.68595 18.0357 10.6458 16.7962 11.3292C15.6193 11.9774 13.9159 12.3351 11.9996 12.3351C10.0832 12.3351 8.3799 11.9782 7.20294 11.3292C5.96348 10.6466 5.31126 9.68595 5.21421 8.4029L5.21586 5.47408C5.21586 4.33003 6.1362 3.39899 7.26545 3.39899L7.26627 3.39817ZM18.1879 18.1434C17.8013 18.5341 17.2872 18.7496 16.7403 18.7496H7.2597C6.71357 18.7496 6.19953 18.5341 5.81215 18.1434C5.42312 17.7503 5.20928 17.228 5.2101 16.6712L5.21339 11.5809C5.58268 11.9486 6.02106 12.2743 6.52852 12.5539C8.29765 13.5285 10.4805 13.7333 12.0004 13.7333C13.5203 13.7333 15.7032 13.5285 17.4723 12.5539C17.9798 12.2743 18.4181 11.9486 18.7874 11.5809L18.7907 16.6712C18.7907 17.2272 18.5761 17.7503 18.1879 18.1434Z" fill="#000000"/>
 </svg>'''
+
+max_errors = 3
 
 stations_names = [" "] + sorted(truth_table["Nom arret"].tolist())
 st.session_state.stations_names = stations_names
@@ -143,6 +148,22 @@ def give_up():
   st.session_state.end_game = True
   st.rerun()
 
+def save_game_stats():
+    stats = {}
+    for letter_id in col_letters + row_letters:
+       stats[f"Critere {letter_id}"] = st.session_state.grid[f"Criteria {letter_id}"]
+    for col_id in col_letters:
+       for row_id in row_letters:
+          stats[f"Arret {col_id+row_id}"] = st.session_state.user_answers[col_id+row_id]
+    
+    stats["Temps"] = str(st.session_state.game_duration)
+    
+    stats_cols = [f"Critere {i}" for i in col_letters+row_letters] + [f"Arret {col_id+row_id}" for col_id in col_letters for row_id in row_letters] + ["Temps"]
+    
+    stats_df = pd.DataFrame([stats], columns=stats_cols, dtype=str)
+    
+    stats_df.to_csv(stats_save_file, sep=";", mode="a", encoding="utf-8", header=False, index=False)
+
 @st.dialog("Bienvenue")
 def welcome():
     
@@ -213,9 +234,8 @@ def check_answers(cell_id):
 def bravo():
   st.balloons()
   st.session_state.clickable_ids = clickable_ids
-  game_duration = time.gmtime(st.session_state.game_duration)
   st.markdown(f'''
-  Grille complétée en **{game_duration.tm_min} min** et **{game_duration.tm_sec} s** bravo !
+  Grille complétée en **{time.gmtime(st.session_state.game_duration).tm_min} min** et **{time.gmtime(st.session_state.game_duration).tm_sec} s** bravo !
   
   ''')
   win_cols = st.columns(4)
@@ -283,23 +303,15 @@ elif max_len <= 45:
 else:
     uniform_font_size = "clamp(0.36rem, 1.6vw, 0.55rem)"
 
-# # --- Lecture du clic précédent ---
-# clicked_id = st.query_params.get("cell")
-# if clicked_id is not None:
-#     col_row = grid_ids_to_letters[int(clicked_id)]
-#     select_station(col_row[0], col_row[1])
-#     st.query_params.clear()  # évite de re-déclencher au prochain rerun
-
-# --- Couleurs par type de case ---
-color_naodoku_bg     = "#b9f2c4"  # case titre NAODOKU
+color_naodoku_bg     = "#b9f2c4"  
 color_naodoku_text   = "#1a5c2e"
-color_criteria      = "#eef0f4"  # critères ligne/colonne
+color_criteria      = "#eef0f4"  
 color_criteria_text = "#333"
 color_criteria_border = "#000000"
-color_empty         = "#FFFFFF"  # cases à remplir (déjà votre couleur actuelle)
+color_empty         = "#FFFFFF"  
 color_empty_text    = "#000000"
 color_empty_border = "#2E7D32"
-color_filled        = "#b9f2c4"  # cases remplies par le joueur
+color_filled        = "#b9f2c4"  
 color_filled_text   = "#1a5c2e"
 color_filled_border = "#2E7D32"
 
@@ -334,7 +346,6 @@ for i in clickable_ids:
         border: 1px solid {color_empty_border} !important;
     }}"""
 
-# --- CSS : force la grille à ne jamais s'empiler + style des cellules ---
 st.markdown(f"""
 <style>
 :root {{
@@ -419,7 +430,6 @@ if st.session_state.first_opening:
     welcome()
     st.session_state.first_opening = False
 
-# --- Génération de la grille : 4 lignes de 4 colonnes ---
 for row in range(5):
     cols = st.columns(4, gap="small")
     if row == 0:
@@ -448,7 +458,7 @@ for row in range(5):
                         else :
                             select_station(i)
 
-err_cols = st.columns(4, gap="small")
+err_cols = st.columns(max_errors +1, gap="small")
 if cols[0].button("**Erreurs**", key="errors_button", disabled=True):
     pass
 errors_labels = [":x:" if st.session_state.errors > i else "" for i in range(3)]
@@ -462,8 +472,9 @@ if cols[3].button(errors_labels[2], key="3rd_error", disabled=True):
 if (len(st.session_state.user_answers) == 9) & (not st.session_state.end_game):
   st.session_state.end_game = True
   st.session_state.game_duration = time.time() - st.session_state.game_start_time
+  save_game_stats()
   bravo()
 
-if (st.session_state.errors == 3) & (not st.session_state.end_game):
+if (st.session_state.errors == max_errors) & (not st.session_state.end_game):
   st.session_state.end_game = True
   you_lose()
